57bd409998c145a739ccf9dfd6e4e7528d4faf94
Christian Zenaty <cazenaty@yahoo.com>
2018-04-25 21:52:50 -0700

    adding mongoose and config files

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+
+const app = express();
+
+// === Mongoose === //
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI);
+
+// === Express Middleware === //
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// === ROUTES === //
+require("./routes/index")(app);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Serving on ${PORT}`);
+});
