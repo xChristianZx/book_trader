1ebbe369756c0d1544d86b11579da89d0820a3f0
Christian Zenaty <cazenaty@yahoo.com>
2018-04-30 21:39:50 -0700

    changing Book model 'name' to 'title'

--- models/Book.js
+++ models/Book.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const BookSchema = new Schema({
-  name: { type: String, required: true, trim: true },
+  title: { type: String, required: true, trim: true },
   author: { type: String, required: true, trim: true },
   bookOwner: { type: Schema.Types.ObjectId, ref: "User" },
   imageCover: { type: String, required: false }
--- seed.js
+++ seed.js
@@ -3,34 +3,34 @@ const Book = require("./models/Book");
 
 const books = [
   {
-    name: "Catch-22",
+    title: "Catch-22",
     author: "Joseph Heller",
     bookOwner: null,
     imageCover:
       "https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Catch22.jpg/220px-Catch22.jpg"
   },
   {
-    name: "The Grapes of Wrath",
+    title: "The Grapes of Wrath",
     author: "John Steinbeck",
     bookOwner: null,
     imageCover:
       "https://upload.wikimedia.org/wikipedia/en/thumb/1/1f/JohnSteinbeck_TheGrapesOfWrath.jpg/200px-JohnSteinbeck_TheGrapesOfWrath.jpg"
   },
   {
-    name: "The Catcher in the Rye",
+    title: "The Catcher in the Rye",
     author: "J.D. Salinger",
     bookOwner: null,
     imageCover:
       "https://images-na.ssl-images-amazon.com/images/I/81OthjkJBuL.jpg"
   },
   {
-    name: "The Great Gatsby",
+    title: "The Great Gatsby",
     author: "F. Scott Fitzgerald",
     bookOwner: null,
     imageCover: "https://upload.wikimedia.org/wikipedia/en/thumb/f/f7/TheGreatGatsby_1925jacket.jpeg/220px-TheGreatGatsby_1925jacket.jpeg"
   },
   {
-    name: "1984",
+    title: "1984",
     author: "George Orwell",
     bookOwner: null,
     imageCover: "https://images-na.ssl-images-amazon.com/images/I/514CVwOrybL.jpg"
