3a6ab0aa4c7094ed9cac3076775ce2a3d7e9a9c5
Christian Zenaty <cazenaty@yahoo.com>
2018-04-30 17:34:59 -0700

    adding cover images to model

--- models/Book.js
+++ models/Book.js
@@ -4,7 +4,8 @@ const { Schema } = mongoose;
 const BookSchema = new Schema({
   name: { type: String, required: true, trim: true },
   author: { type: String, required: true, trim: true },
-  bookOwner: { type: Schema.Types.ObjectId, ref: "User" }
+  bookOwner: { type: Schema.Types.ObjectId, ref: "User" },
+  imageCover: { type: String, required: false }
 });
 
 module.exports = mongoose.model("Book", BookSchema);
