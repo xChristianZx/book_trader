8f8f478d4b7f721f44ab7103c898ddad5cbf2014
Christian Zenaty <cazenaty@yahoo.com>
2018-05-24 15:05:28 -0700

    adding isLoggedIn and rename user properties

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -7,7 +7,13 @@ import {
   LOGOUT_USER_FAIL
 } from "../actions/types";
 
-const initialState = { user: null, isFetching: false, error: false, msg: null };
+const initialState = {
+  error: false,
+  isFetching: false,
+  isLoggedIn: false,
+  msg: null,
+  userInfo: null
+};
 
 export default function(state = initialState, action) {
   switch (action.type) {
@@ -15,10 +21,21 @@ export default function(state = initialState, action) {
     case LOGOUT_USER_REQUEST:
       return { ...state, isFetching: true, error: false, msg: null };
     case LOGIN_USER_SUCCESS:
-      return { ...state, isFetching: false, user: action.payload };
+      return {
+        ...state,
+        isFetching: false,
+        isLoggedIn: true,
+        userInfo: action.payload
+      };
     case LOGIN_USER_FAIL:
     case LOGOUT_USER_FAIL:
-      return { ...state, isFetching: false, error: true, msg: action.error };
+      return {
+        ...state,
+        isFetching: false,
+        isLoggedIn: false,
+        error: true,
+        msg: action.error
+      };
     case LOGOUT_USER_SUCCESS:
       return { ...state, ...initialState };
     default:
