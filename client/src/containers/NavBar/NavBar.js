c77270bcfc927e8dd54d41c82a944dd4e1e6df47
Christian Zenaty <cazenaty@yahoo.com>
2018-05-25 06:59:44 -0700

    Adding Materialize

--- client/src/containers/NavBar/NavBar.js
+++ client/src/containers/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import styles from "./NavBar.css";
-import { Link } from "react-router-dom";
+// import styles from "./NavBar.css";
+import { NavLink } from "react-router-dom";
 import { userLogout } from "../../actions/index";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
@@ -12,19 +12,27 @@ class NavBar extends Component {
       case true:
         return [
           <li key="1">
-            <p>Welcome {userInfo.name}</p>
+            <span>Welcome {userInfo.name}</span>
           </li>,
           <li key="2">
-            <Link to="/user">User</Link>
+            <NavLink to="/books">Books</NavLink>
           </li>,
           <li key="3">
-            <p onClick={this.props.userLogout}>Logout</p>
+            <NavLink to="/user">User</NavLink>
+          </li>,
+          <li key="4">
+            <span onClick={this.props.userLogout}>Logout</span>
           </li>
         ];
       default:
         return [
-          <li key="4">
-            <a href="http://localhost:5000/login">Login/ Register</a>
+          <li key="5">
+            <NavLink to="/books">Books</NavLink>
+          </li>,
+          <li key="6">
+            <a className="btn waves-effect" href="http://localhost:5000/login">
+              Login/ Register
+            </a>
           </li>
         ];
     }
@@ -32,17 +40,16 @@ class NavBar extends Component {
 
   render() {
     return (
-      <header className={styles.wrapper}>
-        <div className={styles.container}>
-          <div>
-            <Link to="/">JungleBooks</Link>
+      <nav className={`blue-grey darken-2`}>
+        <div className={`nav-wrapper container`}>
+          <div className="brand-logo left col s12 m3">
+            <NavLink to="/">Mojave</NavLink>
           </div>
-          <div className={styles.navlinks} >
-            <Link to="/books">Books</Link>
-            <ul>{this.renderNavLinks()}</ul>
+          <div className={`right col s12 m9`}>
+            <ul className={``}>{this.renderNavLinks()}</ul>
           </div>
         </div>
-      </header>
+      </nav>
     );
   }
 }
