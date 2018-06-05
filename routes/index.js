9ac69cf8c552a2a8144c41831f993338b0029392
Christian Zenaty <cazenaty@yahoo.com>
2018-06-05 14:04:52 -0700

    [WIP] Updating add book to include bookOwner _id

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Book = require("../models/Book");
 const chalk = require("chalk");
+const User = require("../models/User");
 
 module.exports = app => {
   // === Get all books === //
@@ -11,21 +12,44 @@ module.exports = app => {
       } else {
         res.send(books);
       }
-    });
+    }).populate("bookOwner", "");
   });
 
   // === Add a book === //
-  app.post("/books/add", (req, res) => {
-    const newBook = req.body;
-    console.log(req.body);
+  app.post("/books/add", async (req, res) => {
+    const { title, author, coverImageURL } = req.body;
+    const { sub } = req.user._json;
+    // console.log(req.body);
+    // console.log("USER:", req.user._json);
+    const { _id } = await findUser_id(sub);
+    // console.log("user_id", _id);
+    const newBook = {
+      title,
+      author,
+      coverImageURL,
+      bookOwner: _id
+    };
     Book.create(newBook, (err, book) => {
       if (err) {
         throw err;
       } else {
-        res.send(chalk.cyan(`${newBook.title} has been added`));
+        /*  
+        TODO - Need to add Book _id to User Model 
+        User.update()
+        */
+        res.send(`${newBook.title} has been added`);
       }
     });
     // Save for testing without adding Book
     // res.send(`${newBook.title} has been added`);
   });
 };
+
+function findUser_id(userSubID) {
+  return User.findOne({ id: userSubID }, "_id", (err, user) => {
+    if (err) {
+      throw err;
+    }
+    console.log("USER FOUND 2", user._id);
+  });
+}
